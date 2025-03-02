affbe78d0409aea95e9df9d07d82794e9ab8b239
Abdulla73 <abd679405@gmail.com>
2025-03-02 20:14:09 +0600

    Added authentication with Laravel Breeze

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Welcome</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 
-    <h1>hello world</h1>
+    <h1>Welcome to My App</h1>
+
+    @auth
+        <p>Hi, {{ auth()->user()->name }}! You're logged in.</p>
+        <a href="{{ route('dashboard') }}">Go to Dashboard</a>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit">Logout</button>
+        </form>
+    @else
+        <a href="{{ route('login') }}">Login</a> |
+        <a href="{{ route('register') }}">Register</a>
+    @endauth
+
 </body>
 </html>
